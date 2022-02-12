aec500ecb2eb0fc870ff24b51373541db30fdad1
meep-m0rp <back.up.email.158@gmail.com>
2022-02-12 18:10:01 -0500

    Enabled automatic binary changing in make file after project creation

--- src/main.lisp
+++ src/main.lisp
@@ -2,7 +2,12 @@
 
 (defun mkproj (name)
   (format t "Creating project ~S~%" name)
-  (uiop:run-program (format nil "tar -xvf /bin/lpj_assets/template.tar && mv template ~S" name)))
+  (uiop:run-program (format nil "tar -xvf /bin/lpj_assets/template.tar"))
+  (uiop:run-program (format nil "mv template ~S" name))
+  (uiop:run-program (concatenate 'string 
+                                 (concatenate 'string "sed -i 's/bin\\/template/bin\\/" name) 
+                                 (concatenate 'string 
+                                              (concatenate 'string "/g' " name) "/Makefile"))))
 
 (defun nil-name ()
   (format t "Invalid project name provided!~%"))
